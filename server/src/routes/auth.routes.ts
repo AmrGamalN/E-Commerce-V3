42faf62bb0f31dc03ea6342f064c56bc2cc7ff2c
AmrGamalN <amr5179520@gmail.com>
2025-02-20 20:30:21 +0200

    Create user login and cookie

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -8,6 +8,11 @@ router.post("/register", async (req: Request, res: Response) => {
   await controller.registerUser(req, res);
 });
 
+// Login user
+router.post("/login", async (req: Request, res: Response) => {
+  await controller.login(req, res);
+});
+
 // Register a new user
 router.get("/verify-email", async (req: Request, res: Response) => {
   await controller.verifyEmail(req, res);
